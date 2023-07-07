2a9bfc171720d7425b65eac4777dc92c858fff84
MajorPayne95 <dbhunter4@gmail.com>
2023-07-06 21:18:42 -0500

    Corrected bug in read/unread and made dynamic DOM Turned read/unread into radio w/ only 1 choice allowed Set limits on allowed input parameters Library is in an acceptable working state for this assignment COMPLETE

--- main.js
+++ main.js
@@ -14,39 +14,54 @@ function addBookToLibrary() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
-  let status = document.getElementById("status").value;
+  let status;
+// this checks which radio button for book read status was selected
+  if (document.getElementById('read').checked) {
+    status = document.getElementById("read").value;
+  } else if (document.getElementById('unread').checked) {
+    status = document.getElementById("unread").value;
+  }
+// this prevents an empty book from being entered
+  if (title == "" || author == "") {
+    alert("Please fill out entire form")
+    return;
+  }
+// generates a position value to be used to identify each book added
   let position = myLibrary.length + 1;
+// Created a new book using the above variables
   let book = new Book(title, author, pages, status, position);
+// Pushes newly created book to myLibrary array
   myLibrary.push(book);
+// Runs the bookElements function to get book to interface
   bookElements(book);
-  console.log(myLibrary);
 }
 
+// This is a master function that runs each small function to get the entered data to the DOM
 function bookElements(book) {
   bookDiv(book);
   bookList(book);
   bookTitle(book);
   bookAuthor(book);
   bookPages(book);
-  bookStatus(book);
-  removeBtn(book);
+  //bookStatus(book);
   statusToggle(book);
+  removeBtn(book);
 }
-
+// This function generates a new div for the books data to be pushed to
 function bookDiv(book) {
   let count = book.position
   let currDiv = document.createElement('div');
   currDiv.className = ("book" + count);
-  document.querySelector(".bookshelf").appendChild(currDiv);
+  document.querySelector(".bookshelf").appendChild(currDiv); // This line appends the created div to the already existing .bookshelf element
 }
-
+// This function generates a new ul for each books li elements to go in
 function bookList(book) {
   let count = book.position
   let currList = document.createElement('ul');
   currList.className = ('bookInfo' + count);
   document.querySelector(".book" + count).appendChild(currList);
 }
-
+// The below functions generate an li element for each input variable the user submitted
 function bookTitle(book) {
   let count = book.position
   let bookTitle = document.createElement('li');
@@ -58,7 +73,7 @@ function bookTitle(book) {
 function bookAuthor(book) {
   let count = book.position
   let bookAuthor = document.createElement('li');
-  let currAuthor = document.createTextNode(book.author);
+  let currAuthor = document.createTextNode('Author: ' + book.author);
   bookAuthor.appendChild(currAuthor)
   document.querySelector(".bookInfo" + count).appendChild(bookAuthor);
 }
@@ -66,18 +81,12 @@ function bookAuthor(book) {
 function bookPages(book) {
   let count = book.position
   let bookPages = document.createElement('li');
-  let currPages = document.createTextNode(book.pages);
+  let currPages = document.createTextNode('Page count: ' + book.pages);
   bookPages.appendChild(currPages)
   document.querySelector(".bookInfo" + count).appendChild(bookPages);
 }
+// end of user input DOM generation functions
 
-function bookStatus(book) {
-  let count = book.position
-  let bookStatus = document.createElement('li');
-  let currStatus = document.createTextNode(book.status);
-  bookStatus.appendChild(currStatus)
-  document.querySelector(".bookInfo" + count).appendChild(bookStatus);
-}
 //function that creates a delete input on each book
 function removeBtn(book) {
   let count = book.position;
@@ -86,42 +95,39 @@ function removeBtn(book) {
   removeBook.className = ('removeBtn')
   document.querySelector('.book' + count).appendChild(removeBook);
 }
-
-let readUnread = document.createElement('button');
-
+// Function that creates a read/unread toggle for the user to dynamically change a given books status
 function statusToggle(book) {
   let count = book.position;
-  readUnread.innerText = 'Read';
-  readUnread.className = ('statusToggle')
+  let status = book.status
+  let readUnread = document.createElement('button');
+  readUnread.innerText = status;
+  readUnread.className = ('statusToggle' + count)
   document.querySelector('.book' + count).appendChild(readUnread);
 }
-
+// Assigns a DOM element to a usable variable to be used for a click event
 const divElement = document.querySelector(".bookshelf");
 divElement.addEventListener('click', deleteBook);
-
+// Function that runs when a Remove Book button is pressed
 function deleteBook(e) {
-  if(!e.target.classList.contains('removeBtn')) {
+  if(!e.target.classList.contains('removeBtn')) { // this ignores all clicks that don't contain the .removeBtn class 
     return;
   } else {
     console.log('remove clicked');
+    // When .removeBtn class is detected this sets its value to a variable and removes the nearest div, which is always the container of the given book
     let btn = e.target;
     btn.closest("div").remove();
   }
 }
-
+// Uses the same div variable to create another click event for read/unread status
 divElement.addEventListener('click', changeStatus);
-
-function changeStatus(e) {
-  if(!e.target.classList.contains('statusToggle')) {
-    return;
-  }
-  
-  if (readUnread.innerText == "Read") {
-    readUnread.innerText = "Unread"
-    return;
-  } else if (readUnread.innerText == "Unread") {
-    readUnread.innerText = "Read"
-    return;
+// This function sets the clicked button to a variable that is then checked for a read/unread DOM text
+function changeStatus(e) { 
+  let current = e.target
+  // This sets the clicked read/unread buttons text to the opposite value
+  if (current.innerText == "Read") {
+    current.innerText = "Unread"
+  } else if (current.innerText == "Unread") {
+    current.innerText = "Read"
   }
 }
 
